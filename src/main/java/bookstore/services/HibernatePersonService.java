c1aa3804ce3c5bf9fd4f547fa5038f9c0e9280ce
Jeka1978 <Kit2009>
2013-06-29 01:23:45 +0300

    add person entity

---
+++ src/main/java/bookstore/services/HibernatePersonService.java
@@ -0,0 +1,27 @@
+package mybookstore.services;
+
+import mybookstore.dao.PersonDao;
+import mybookstore.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jeka
+ * Date: 6/28/13
+ * Time: 11:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class HibernatePersonService implements PersonService {
+    @Autowired
+    private PersonDao personDao;
+
+    @Override
+    @Transactional
+    public void savePerson(Person person) {
+        personDao.savePerson(person);
+
+    }
+}
