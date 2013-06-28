de9379e8be54ce24c5ca8524c4f880363546ff23
JBaruch <jbaruch@sadogursky.com>
2013-06-29 01:53:23 +0300

    test to JUnit

--- src/test/java/bookstore/services/PersonServiceTest.java
+++ src/test/java/bookstore/services/PersonServiceTest.java
@@ -2,15 +2,17 @@ package bookstore.services;
 
 import bookstore.model.Book;
 import bookstore.model.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.testng.annotations.Test;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author jbaruch
  * @since 29/06/13
  */
-
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/hibernate-context.xml"})
 public class PersonServiceTest {
     @Autowired
